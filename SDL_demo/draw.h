feda2de62f71f2b0df4a3f092f0b6f71e4e2c68c
Alexandr Gaponenko <shura.gaponenko.01@mail.ru>
2024-07-11 19:18:51 +0300

    Revert "Loading project"
    This reverts commit 87e5a6f06dd0372a18d52b303af4b0f2a563bda5.

--- SDL_demo/draw.h
+++
@@ -1,9 +0,0 @@
-#ifndef DRAW_H
-#define DRAW_H
-
-#include "stdafx.h"
-
-//Rectangle clippingRect;
-void DrawScene(SDL_Renderer *renderer, double angle_master, double angle_slave, int size_x, int size_y, int master_x1, int master_y1, int slave_x1, int slave_y1);
-
-#endif
